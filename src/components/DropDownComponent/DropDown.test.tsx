5205f2d9b2bfa29a607cfe74e20879d19a2838cf
W4ddNJ5n76JHfk54QOyaCg9L0jqj6VZTziVleuK6yrw= <Falcon_Code_user+1-at-282572457531>
2024-04-06 11:32:24 +0530

    added e2e tests, dropdown unit test

--- src/components/DropDownComponent/DropDown.test.tsx
+++ src/components/DropDownComponent/DropDown.test.tsx
@@ -1,85 +1,207 @@
-import "@testing-library/jest-dom";
-import { fireEvent, render} from '@testing-library/react';
+import { fireEvent, render, waitFor} from '@testing-library/react';
 import  {DropDown}  from './DropDown';
+import "@testing-library/jest-dom";
 
+describe('DropDown Component', () => {
 const optionList = [
     { value: "option1", label: "Option 1" },
     { value: "option2", label: "Option 2" },
-    { value: "option3", label: "Option 3" },
-];
+    ];
     
-const compulsoryList = [
+    const compulsoryList = [
     { value: "compulsory1", label: "Compulsory 1", isFixed: true },
     { value: "compulsory2", label: "Compulsory 2", isFixed: true },
-];
+    ];
 
 const registerMock = jest.fn();
 const validationSchemaMock = jest.fn();
 const errorsMock = {};
 
+//checks normal render
+it('renders dropdown component', () => {
+const {getByRole} = render(
+        <DropDown
+        name="test"
+        optionList={optionList}
+        compulsoryList={compulsoryList}
+        size='small'
+        existingDataList={optionList || []}
+        register={registerMock}
+        validationSchema={validationSchemaMock}
+        errors={errorsMock}
+        onchange={() => {}}
+        placeholder='abc'
+        />
+    );
 
-//a functional drop down component
-describe('DropDown Component', () => {
+    //check if the combobox got rendered
+    const dropdown = getByRole('combobox');
+    expect(dropdown).toBeTruthy();
+})
 
-    //check basic rendering
-    it('renders select box', () => {
-         const { queryByRole, queryByText} = render(
-            <DropDown
-                name="test"
-                optionList={optionList}
-                compulsoryList={compulsoryList}
-                size="small"
-                existingDataList={optionList || []}
-                register={registerMock}
-                validationSchema={validationSchemaMock}
-                errors={errorsMock}
-                onchange={() => {}}
-                placeholder="abc"
-            />
-        );
-        //check if dropdown component is present
-        expect(queryByRole("combobox")).toBeInTheDocument();
+//check if all compulsory options are already present in the combobox
+it('Selecting an option', async () => {
+    const mockOnChange = jest.fn();
 
-        //check if all options are present in the dropdown component
-        expect(queryByText("Option 1")).toBeInTheDocument()
-        expect(queryByText("Option 2")).toBeInTheDocument()
-        expect(queryByText("Option 3")).toBeInTheDocument()
+    const {queryByText} = render(
+        <DropDown
+            name="test"
+            optionList={optionList}
+            compulsoryList={compulsoryList}
+            register={registerMock}
+            validationSchema={validationSchemaMock}
+            errors={errorsMock}
+            onchange={mockOnChange}
+        />
+    );
 
-        //check if all compulsory options are present
-        expect(queryByText("Compulsory 1")).toBeInTheDocument()
-        expect(queryByText("Compulsory 2")).toBeInTheDocument()
-    })
-
-    //check if the drop down options are selectable
-    it('DropDown component selects an option', async () => {
-        const { getByRole, getByText} = render(
-            <DropDown
-                name="test"
-                optionList={optionList}
-                compulsoryList={compulsoryList}
-                size="small"
-                existingDataList={optionList || []}
-                register={registerMock}
-                validationSchema={validationSchemaMock}
-                errors={errorsMock}
-                onchange={() => {}}
-                placeholder="abc"
-            />
-        );
-        // Open the dropdown menu
-        const dropdown = getByRole("combobox");
-        const control = dropdown.querySelector(".css-yk16xz-control");
-
-        if (control) {
-            fireEvent.mouseDown(control);
-
-            // Select an option from the menu
-            fireEvent.click(getByText("Option 1"));
-
-            // Check if onchange function is called with the correct value
-            expect(onchange).toHaveBeenCalledWith("compulsory1, option1");
-        }
-
+    //check if compulsory values are already present in the combobox
+    compulsoryList.forEach((compulsoryOption) => {
+        const optionElement = queryByText(compulsoryOption.label);
+        expect(optionElement).toBeInTheDocument();
     });
 });
 
+//check if all non-compulsory options are not already present in the combobox
+it('Selecting an option', async () => {
+    const mockOnChange = jest.fn();
+
+    const {queryByText} = render(
+        <DropDown
+            name="test"
+            optionList={optionList}
+            compulsoryList={compulsoryList}
+            register={registerMock}
+            validationSchema={validationSchemaMock}
+            errors={errorsMock}
+            onchange={mockOnChange}
+        />
+    );
+
+    //check if compulsory values are not already present in the combobox
+    optionList.forEach((nonCompulsoryOption) => {
+        const optionElement = queryByText(nonCompulsoryOption.label);
+        expect(optionElement).not.toBeInTheDocument();
+    });
+});
+
+//checks if predefined option could be selected
+it('Selecting an option', async () => {
+    const mockOnChange = jest.fn();
+
+    const {getByRole, getByText} = render(
+        <DropDown
+            name="test"
+            optionList={optionList}
+            compulsoryList={compulsoryList}
+            register={registerMock}
+            validationSchema={validationSchemaMock}
+            errors={errorsMock}
+            onchange={mockOnChange}
+        />
+    );
+
+    // Open the dropdown menu
+    const dropdown = getByRole("combobox");
+
+    //click on the dropdown
+    fireEvent.focus(dropdown);
+    fireEvent.keyDown(dropdown, {key:"ArrowDown", code:"ArrowDown"})
+
+    //get the option
+    const optiontoselect = getByText("Option 1");
+    fireEvent.click(optiontoselect)
+
+    //check if onchange took place
+    expect(mockOnChange).toHaveBeenCalledWith('compulsory1, compulsory2, option1');
+    //it already contains compulsory1,2 because theyre compulsory lists items
+});
+
+//checks if user-created option get added
+it('Creating an option', async () => {
+    const mockOnChange = jest.fn();
+    const {getByRole, getByText} = render(
+        <DropDown
+            name="test"
+            optionList={optionList}
+            compulsoryList={compulsoryList}
+            register={registerMock}
+            validationSchema={validationSchemaMock}
+            errors={errorsMock}
+            onchange={mockOnChange}
+        />
+    );
+
+    // Open the dropdown menu
+    const dropdown = getByRole("combobox");
+
+    //insert the new option in the inputfield of combobox
+    fireEvent.focus(dropdown);
+    fireEvent.keyDown(dropdown, {key:"ArrowDown", code:"ArrowDown"}) //focus on the combobox
+    fireEvent.change(dropdown,{target:{value:"option100"}})//enter value
+    fireEvent.click(getByText("Add new option \"option100\"")) //create new option
+    await waitFor(()=>getByText("option100")) //wait for value to appear
+
+    //check if onchange took place with option100
+    expect(mockOnChange).toHaveBeenCalledWith('compulsory1, compulsory2, option100');
+    //it already contains compulsory1,2 because they are compulsory lists items
+});
+
+//check if compulsory option can be removed
+it('Removing a compulsory option', async () => {
+    const mockOnChange = jest.fn();
+    const {getByLabelText} = render(
+        <DropDown
+            name="test"
+            optionList={optionList}
+            compulsoryList={compulsoryList}
+            register={registerMock}
+            validationSchema={validationSchemaMock}
+            errors={errorsMock}
+            onchange={mockOnChange}
+        />
+    );
+
+    //click on remove button of compulsory option
+    fireEvent.click(getByLabelText("Remove Compulsory 1"))
+
+    //check if onchange did not take execute
+    expect(mockOnChange).not.toHaveBeenCalled();
+});
+
+//check if non-compulsory options can get removed
+it('Removing a non-compulsory option', async () => {
+    const mockOnChange = jest.fn();
+    const {getByRole, getByLabelText, getByText} = render(
+        <DropDown
+            name="test"
+            optionList={optionList}
+            compulsoryList={compulsoryList}
+            register={registerMock}
+            validationSchema={validationSchemaMock}
+            errors={errorsMock}
+            onchange={mockOnChange}
+        />
+    );
+
+    const dropdown = getByRole("combobox");
+    
+    //click on the dropdown
+    fireEvent.focus(dropdown);
+    fireEvent.keyDown(dropdown, {key:"ArrowDown", code:"ArrowDown"})
+
+    //click on the non-compulsory option
+    const optiontoselect = getByText("Option 1");
+    fireEvent.click(optiontoselect)
+    
+    //check if option got appended
+    expect(mockOnChange).toHaveBeenCalledWith('compulsory1, compulsory2, option1');
+
+    //click on remove button of non-compulsory option
+    fireEvent.click(getByLabelText("Remove Option 1"))
+
+    //check if onchange took place without option1
+    expect(mockOnChange).toHaveBeenCalledWith('compulsory1, compulsory2');
+});
+
+});
